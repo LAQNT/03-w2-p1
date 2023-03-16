654331a69504bc0efff877b2000b3db60449191d
Laura Quintero Agudelo <laura.quintero.a@hotmail.com>
2023-03-16 18:42:57 +0100

    almost completed

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// -----footerTotalAlbums---------
 
 function totalAlbums() {
     let arrayAlbums = document.querySelectorAll('.carousel img');
@@ -11,4 +12,62 @@ function totalAlbums() {
     return spanText.innerText = 'You listened a total of ' + totalAlbums +' albums';
 }
 
+
+// -----buttonText---------
+
+function buttonText(btn) {
+    
+    if (btn.innerHTML == 'Show') {
+        btn.innerHTML = 'Hide';
+            
+    } else if (btn.innerHTML == 'Hide'){
+        btn.innerHTML = 'Show';
+    }
+    
+
+    return btn
+}
 console.log(totalAlbums());
+
+
+
+// -----addGen---------
+
+function albumGenre() {
+
+    let favAlbumCard = document.querySelectorAll('#topAlbums .card-img');
+
+    let genre = 'new';
+
+    
+    for (card of favAlbumCard) {
+        let nSpan = document.createElement('span');
+        nSpan.innerHTML=genre;
+        card.appendChild(nSpan);
+    }
+
+    return 
+}
+
+console.log(albumGenre());
+
+
+
+// -----removeSong---------
+
+function deleteTableRow(btn) {
+    btn.parentNode.parentNode.remove()
+} 
+
+
+// -----addSong---------
+
+function addSong() {
+
+}
+
+
+
+    
+
+
